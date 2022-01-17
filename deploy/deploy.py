11e2a305f57362cbcdc8ff4ee8c1c8a5907337bc
tsamuel33 <tsamuel86@gmail.com>
2022-01-17 01:31:58 -0500

    adjusted default capability for update function

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -188,10 +188,7 @@ def main():
         max_count = TIMEOUT_SECONDS / CF_CHECK_PERIOD_SECONDS
         templatename = update['Template']
         stackname = update['StackName']
-        if templatename == 'iam.template':
-            templatecapability = 'CAPABILITY_NAMED_IAM'
-        else:
-            templatecapability = ''
+        templatecapability = 'CAPABILITY_NAMED_IAM'
         templatelocation = str(TEMPLATE_DIR) + '/' + templatename
         cf_template_yaml = load_template(templatelocation)
         try:     
